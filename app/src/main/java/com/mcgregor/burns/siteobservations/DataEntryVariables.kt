44d6cb6d8314ef83af389ae78cee288d2602338f
Andrew McGregor <burnsmcgregor@outlook.com>
2019-07-04 20:18:00 +0100

    Site Observations - Added al Spinner Adapters

--- app/src/main/java/com/mcgregor/burns/siteobservations/DataEntryVariables.kt
+++ app/src/main/java/com/mcgregor/burns/siteobservations/DataEntryVariables.kt
@@ -1,16 +1,86 @@
 package com.mcgregor.burns.siteobservations
 
-enum class Severity{
+enum class Severity {
     Low, Medium, High
 }
 
 val conditions = listOf<String>("unsafe Act", "Unsafe Condition", "Near miss", "Significant Near Miss")
 
-val trades: List<String> = listOf<String>("Akela","Bunyand and Bell","Dovend","Turner Access","Marley","Kelvin","Tilecraft",
-        "Tclarke","Pbr","A Boyd","Bricklayers","Universal","Voltaire","Landscape Design","Flooring Contractor",
-        "Jdr","Energy Assets","Apex","Cleaner")
+val contractors: List<String> = listOf<String>(
+    "Akela", "Bunyand and Bell", "Dovend", "Turner Access", "Marley", "Kelvin", "Tilecraft",
+    "Tclarke", "Pbr", "A Boyd", "Bricklayers", "KS Windows", "Universal", "Voltaire", "Landscape Design", "Flooring Contractor",
+    "Jdr", "Energy Assets", "Apex", "Cleaner"
+)
 
-val contractors = listOf<String>()
+val trades = listOf<String>(
+    "Bricklayer", "Cala", "Cleaner", "Demolition", "Electricity / Services",
+    "Fencing", "Flooring", "General Builder", "Groundworker", "Housekeeping", "Insulation",
+    "Joiner / Carpenter", "Kit / Timber Frame", "Kitchen", "Landscaper", "Lift", "Mastic",
+    "Other", "Painting & Decorating", "Piling", "Plasterer / Dryliner", "Plumbing", "Roof Tiler",
+    "Roofer", "Roughcastng / Render", "Scaffolding", "Site Security", "Steelworker", "Tiler",
+    "Traffic management", "Utilities", "Wardrobe", "Window Fitter"
+)
 
-val issues = listOf<String>()
+val issues = listOf<String>(
+    "Access / Egress - Inadequate",
+    "Access / Egress - Plot Access",
+    "Access / Egress - Temp Stairs / Handrail",
+    "Electrical - Generally Unsafe",
+    "Electrical - Insufficient Testing",
+    "Electrical - Unsafe System Of Work",
+    "Enviromental - General",
+    "Enviromental - Lack Of Dust Suppression",
+    "Enviromental - No Drip Tray",
+    "Enviromental - Waste Management",
+    "Excavations",
+    "Fire - Equipment Issues",
+    "Fire - Smoking on Site",
+    "General - COSHH",
+    "General - Debris not being removed from work area",
+    "General - Induction",
+    "General - Manual Handling",
+    "General - Materials Storage",
+    "General - Not Signing In",
+    "General - Rams",
+    "General - Unsafe sequence of work",
+    "General - unsafe working practice",
+    "General - Weather",
+    "General - Welfare Cleanliness",
+    "General - Work at height",
+    "General - Workplace tidiness",
+    "Hand Tools - Dust Suppression",
+    "Manhole / Disconnector lids not secured",
+    "Permit to work - Confined spaces",
+    "Permit to work - Hot works",
+    "Permit to work - Overhead cables",
+    "Permit to work - Permit to dig",
+    "Plant - beacon not being used",
+    "Plant - Key left in ignition",
+    "Plant - Machine issue",
+    "Plant - Operator issue",
+    "Plant - Seatbelt not being worn",
+    "PPE - Boots",
+    "PPE - Dust mask",
+    "PPE - Ear Defenders",
+    "PPE - Gloves",
+    "PPE - Googles",
+    "PPE - Hard Hat",
+    "PPE - High Vis",
+    "Public Protection",
+    "Scaffolding - Bombing Materials",
+    "Scaffolding - Brickguards removed",
+    "Scaffolding - Exceeding maximum loads",
+    "Scaffolding - Handrails removed",
+    "Scaffolding - harness",
+    "Scaffolding - Hop ups removed",
+    "Scaffolding - Poor ground conditions",
+    "Scaffolding - Tidiness",
+    "Scaffolding - Unsafe Adjustment",
+    "Scaffolding - Unsafe working practice",
+    "Traffic Management - Banking of wagons / plant",
+    "Traffic Management - Lack of segregation",
+    "Traffic Management - Loading and unloading",
+    "Traffic Management - Public safety",
+    "Traffic Management - walkways inadequate",
+    "Traffic Management - Walkways not being used")
 
--- app/src/main/java/com/mcgregor/burns/siteobservations/MainActivity.kt
+++ app/src/main/java/com/mcgregor/burns/siteobservations/MainActivity.kt
@@ -13,8 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.observation_layout)
 
-        val adapter = ArrayAdapter(applicationContext,0,trades)
-        trade_spinner.adapter = adapter
+        val tradeAdapter = ArrayAdapter(applicationContext,R.layout.spinner_list, trades)
+        val conditionsAdapter = ArrayAdapter(applicationContext,R.layout.spinner_list, conditions)
+        val severityAdapter = ArrayAdapter(this,R.layout.spinner_list, Severity.values())
+        val issuesAdapter = ArrayAdapter(this,R.layout.spinner_list, issues)
+        val contractorsAdapter = ArrayAdapter(this,R.layout.spinner_list, contractors)
+        trade_spinner.adapter = tradeAdapter
+        condition_spinner.adapter = conditionsAdapter
+        severity_spinner.adapter = severityAdapter
+        issue_spinner.adapter = issuesAdapter
+        contractor_spinner.adapter = contractorsAdapter
 
     }
 }
