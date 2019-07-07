af6f0e2d596730219b7fe06cc86facaa4c861f9a
Andrew McGregor <burnsmcgregor@outlook.com>
2019-07-07 15:52:22 +0100

    Site Observations - Added bottom menu

--- app/src/main/java/com/mcgregor/burns/siteobservations/MainActivity.kt
+++ app/src/main/java/com/mcgregor/burns/siteobservations/MainActivity.kt
@@ -2,10 +2,13 @@ package com.mcgregor.burns.siteobservations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.SpinnerAdapter
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.observation_layout.*
 
 
@@ -18,6 +21,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navController = findNavController(R.id.nav_host_fragment)
+
+        bottom_nav.setupWithNavController(navController)
+        bottom_nav.menu.findItem(R.id.dateEntryMenuItem).setOnMenuItemClickListener(MenuItem.OnMenuItemClickListener { item ->
+            navController.navigate(R.id.observationFragment)
+            true
+        })
+
+        bottom_nav.menu.findItem(R.id.dataDisplayMenuItem).setOnMenuItemClickListener(MenuItem.OnMenuItemClickListener { item ->
+            navController.navigate(R.id.observationFragment)
+            true
+        })
+
     }
 
     override fun onSupportNavigateUp(): Boolean = navController.navigateUp() || super.onSupportNavigateUp()
